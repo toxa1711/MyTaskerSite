2620ff53c0fbd3cc655c79fe7f33866e1b797eb6
toxa1711 <akmetony@yandexru>
2021-04-04 01:31:56 +0300

    -php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ru" >
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Название страницы - отображается на вкладке браузера и в поиске</title>
+</head>
+<body>
+<div id = "progress">
+  <div id = "current">
+
+  </div>
+  <div id = "taskDone" style="width: 20px"></div>
+  <div id = "taskDone"></div>
+  <div id = "taskDone"></div>
+  <div id = "taskDone"></div>
+  <div id = "task"></div>
+  <div id = "task"></div>
+  <div id = "task" style="width: 127px"></div>
+</div>
+
+<div id = "story">
+  <h1><s>Sleep22</s></h1>
+  <h1><s>Waking up</s></h1>
+  <h1><s>Univercity</s></h1>
+  <h1><s>Boxing</s></h1>
+  <h1>Task5</h1>
+  <div id = "currentTask">fgh</div>
+
+ <div id = "buttonRed"></div>
+ <div id = "buttonRed"></div>
+ <div id = "buttonRed"></div>
+ <br>
+
+  <h1>Task6</h1>
+  <h1>Task7</h1>
+</div>
+
+</body>
+</html>
